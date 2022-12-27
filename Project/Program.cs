2ce083e27486e4a038d3755ae2b3be02c13676d8
Tatiana443 <tvb399@yandex.ru>
2022-12-27 17:41:57 +0600

    Created project

---
+++ Project/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] array1 = { "hello", "2", "world", ";-)", "1234", "-2" };
+
+void PrintArray(string[] array1)
+{
+    Console.Write("[");
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (i < array1.Length - 1) Console.Write($"{array1[i]}, ");
+        else Console.Write($"{array1[i]}");
+    }
+    Console.Write("]");
+}
+
+void PrintArrayThreeChar(string[] arrayNew)
+{
+    for (int i = 0; i < arrayNew.Length; i++)
+    {
+        Console.Write(arrayNew[i] + " ");
+    }
+}
+
+string[] CreatNewArrayThreeChar(string[] array)
+{
+    string[] arrayNew = new string[array.Length];
+    int temp = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrayNew[temp] = array[i];
+            temp++;
+        }
+    }
+    return arrayNew;
+}
+
+Console.WriteLine(" ");
+PrintArray(array1);
+Console.WriteLine($" --> ");
+string[] arrayNew = CreatNewArrayThreeChar(array1);
+PrintArrayThreeChar(arrayNew);
+
